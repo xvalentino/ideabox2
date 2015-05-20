92028aa157839a4e48e6b91649cc119dba57f89a
Valentino Espinoza <xvalentino13@gmail.com>
2015-05-20 15:48:15 -0600

    flash message is working with ajax

--- app/assets/javascripts/ideas.js
+++ app/assets/javascripts/ideas.js
@@ -1,15 +1,16 @@
 $( document ).ready(function() {
-
   $('tbody tr .idea-destroy').on('click', function() {
     $idea = $(this).parent();
     ideaId =  $idea.data("id");
 
+
     $.ajax({
       url: '/ideas/' + ideaId,
       type: 'DELETE'
-      })
+    })
     .always(function() {
       $idea.remove();
+      $('#flash_message').hide().fadeIn(1000).then.remove();
     });
   });
 });
