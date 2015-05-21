63bb598271ebb879f59bd1c7b5280ea03c51234d
Valentino Espinoza <xvalentino13@gmail.com>
2015-05-21 02:54:03 -0600

    idea quality defaults to swill

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -1,5 +1,12 @@
 class Idea < ActiveRecord::Base
-  enum quality: ["swill", "plausible", "genius"]
+  enum quality: [:swill, :plausible, :genius]
+
   validates :title, presence: true
   validates :body, presence: true
+
+  before_save :set_quality
+
+  def set_quality
+    self.quality = 0
+  end
 end
--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Idea, type: :model do
   describe "Idea" do
     it "has a quality that defaults to swill" do
       idea_attributes = {title: "idea title", body: "idea body"}
-      idea = Idea.new(idea_attributes)
+      idea = Idea.create(idea_attributes)
       expect(idea.quality).to eq("swill")
     end
   end
